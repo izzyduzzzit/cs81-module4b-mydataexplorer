8759db86db9b8705e3e0333c1567117e77c6c4ac
Izarra Villareal <izarravill@gmail.com>
2025-07-20 21:11:12 -0700

    Added initial array constant of myWeek to myDataJournal.js

---
+++ myDataJournal.js
@@ -0,0 +1,12 @@
+// Izarra Villareal CS 81 JavaScript Module 4 Assignment 4B: My Personal Data Objects
+
+const weekData = [
+    { day: "Monday", sleepHours: 7, screenTime: 5, mood: "Happy", caffeineIntake: 2, focusLevel: 8 },
+    { day: "Tuesday", sleepHours: 6, screenTime: 6, mood: "Stressed", caffeineIntake: 3, focusLevel: 6 },
+    { day: "Wednesday", sleepHours: 8, screenTime: 4, mood: "Content", caffeineIntake: 1, focusLevel: 9 },
+    { day: "Thursday", sleepHours: 5, screenTime: 7, mood: "Anxious", caffeineIntake: 4, focusLevel: 5 },
+    { day: "Friday", sleepHours: 7, screenTime: 5, mood: "Happy", caffeineIntake: 2, focusLevel: 8 },
+    { day: "Saturday", sleepHours: 9, screenTime: 3, mood: "Relaxed", caffeineIntake: 1, focusLevel: 9 },
+    { day: "Sunday", sleepHours: 8, screenTime: 4, mood: "Content", caffeineIntake: 1, focusLevel: 8 }
+];
+
